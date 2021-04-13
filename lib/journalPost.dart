c278bce96142f7da262311cdd4c930da57ab0ea6
August Bredberg <augustbredberg@gmail.com>
2021-04-13 13:23:37 +0200

    can add widgets to canvas, drag them and zoom them

--- lib/journalPost.dart
+++ lib/journalPost.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:skillmill_demo/objects/zoomableObject.dart';
 import 'objects/movableObject.dart';
+import 'objects/zoomableObject.dart';
 
 
 class JournalPost extends StatefulWidget{
@@ -21,34 +23,72 @@ List<Widget> movableItems = [];
 
 @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            width: 500,
-            height: 500,
-            child: Stack(
-              alignment: Alignment.center,
-              children: 
-                movableItems,
-                //MoveableStackItem(),
-               //MovableObject(),
-                //TextOverImage(),
-                //Text(''),
-              
-              
-              
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          
+          
+          //mainAxisAlignment: MainAxisAlignment.,
+        
+          children: [
+            
+            Container(
+              margin: EdgeInsets.only(top:25, left:5, right:5, bottom: 0),
+              height: MediaQuery.of(context).size.width * 0.20,      
+              child: Center(
+                child: Column(
+                  children: [
+                    Text("Situation context", style: TextStyle(fontSize: 35)),
+                    Text("Use symbols to describe the context of the situation", style: TextStyle(fontSize: 15)),
+                  ],
+                ),
+              ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {  
-              setState(() {
-                movableItems.add(MoveableStackItem());
-              });
-            },
-            child: Text("nedanför boxern"),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.only(left:8, right:8),
+              height: 400,
+              child: Card(
+                elevation: 10,
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: 
+                    movableItems,
+                  
+                  
+                  
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ElevatedButton(
+              onPressed: () {  
+                setState(() {
+                  movableItems.add(MoveableStackItem());
+                  //movableItems.add(ZoomableObject());
+                });
+              },
+              child: Text("Add Emoji", style: TextStyle(fontSize: 30),),
+            ),
+          ],
+        ),
       ),
     );
  
--- lib/objects/movableObject.dart
+++ lib/objects/movableObject.dart
@@ -4,6 +4,9 @@
 import 'package:flutter/material.dart';
 import 'package:skillmill_demo/objects/pinchableObject.dart';
 import 'package:zoom_widget/zoom_widget.dart';
+import 'package:zoomer/zoomer.dart';
+import 'package:vector_math/vector_math_64.dart' show Vector3;
+
 
 class MoveableStackItem extends StatefulWidget { 
   @override State<StatefulWidget> createState() { 
@@ -14,6 +17,10 @@ class _MoveableStackItemState extends State<MoveableStackItem> {
   double xPosition = 0;
   double yPosition = 0;
   Color color;
+
+  double _scale = 1.0;
+  double _previousScale = 1.0;
+
   @override
   void initState() {
     color = Colors.orange;
@@ -25,21 +32,76 @@ class _MoveableStackItemState extends State<MoveableStackItem> {
       top: yPosition,
       left: xPosition,
       child: GestureDetector(
+        
+        /*
         onPanUpdate: (tapInfo) {
           setState(() {
             xPosition += tapInfo.delta.dx;
             yPosition += tapInfo.delta.dy;
           });
         },
-        child: Center(
-    child:  Container(
-          width: 150,
-          height: 150,
-          color: color,
+        */
+        
+        onScaleStart: (ScaleStartDetails details) {
+          print(details);
+          _previousScale = _scale;
+          
+          setState(() {
+            var offset = details.focalPoint;
+            xPosition = offset.dx;
+            yPosition = offset.dy; 
+          });
+        },
+        onScaleUpdate: (ScaleUpdateDetails details) {
+          
+          //lastRotation += details.rotation;
+          
+
+
+          print(details);
+          
+          //xPosition = details. focalPoint.dx - xPosition; //  .delta.dx;
+          //yPosition = details.focalPoint.dy - yPosition;
+          
+
+          setState(() {
+            var offset = details.focalPoint;
+            xPosition = offset.dx-MediaQuery.of(context).size.width * 0.20;
+            yPosition = offset.dy -MediaQuery.of(context).size.width * 0.37; 
+            _scale = _previousScale * details.scale;
+          });
+        },
+        onScaleEnd: (ScaleEndDetails details) {
+          print(details);
+          _previousScale = 1.0;
+          //xPosition = details.  .localFocalPoint.dx; //  .delta.dx;
+          //yPosition = details.localFocalPoint.dy;
+          setState(() {});
+        },
+        child: RotatedBox(
+            quarterTurns: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Transform(
+                alignment: FractionalOffset.center,
+                transform: Matrix4.diagonal3(Vector3(_scale, _scale, _scale)),
+                child: //Container(height: 100, width: 100, color: Colors.orange,)
+                
+                Icon(Icons.access_alarms, size: 100),
+                //Text("test"),
+              ),
+            ),
         ),
+      
+        
+        
+        /*
+        Center(
+          child:  Text("🤔", style: TextStyle(fontSize: 30),),//Container(decoration: BoxDecoration(color: Colors.green),height: 150,width: 150),
+          */
     
-  ),
       ),
+      
     );
   }
 }
